58871ce0d7195fad3efddf1a3830b7a7a8231de5
Sonu <notifications3972@gmail.com>
2017-10-25 15:41:07 -0700

    Added More Functions - Still Need to Test

--- MarketMe.sol
+++ MarketMe.sol
@@ -1,17 +1,22 @@
 pragma solidity ^0.4.18;
-import 'strings.sol';
+import "github.com/ethereum/dapp-bin/library/stringUtils.sol";
 
 contract MarketMe {
 
     //true false only for now but accepts string, not boolean
     string question;
     mapping(address => string) answer;
+    address[] answerArray;
+    uint payoutQty;
+    uint payoutCounter;
+    uint initialFunding;
 
     //constructor to create question and accept eth
-    function MarketMe(string _question) payable public {
+    function MarketMe(string _question, uint _payoutQty) payable public {
         question = _question;
-        this.balance += msg.value;
         QuestionCreated(_question, this.balance);
+        initialFunding = msg.value;
+        payoutQty = _payoutQty;
     }
 
     event AnswerReceived(
@@ -21,14 +26,39 @@ contract MarketMe {
 
     event QuestionCreated(
         string question,
-        uint ethSent
+        uint ethReceived
     );
+
+    function getQuestion() public constant returns (string _question){
+        _question = question;
+    }
 
     //anyone can answer question. saves answer into mapping
     function answerQuestion(string _answer) public {
+        //verify same person hasn't answered question yet
         if (StringUtils.equal(answer[msg.sender], "")){
+            //add answer into map and push into array and set off event
              answer[msg.sender] = _answer;
+             answerArray.push(msg.sender);
              AnswerReceived(_answer, msg.sender);
+
+             //if this is the last accepted answer, pay with all ether left (assumes gas is used sending previous ether transactions)
+             if (payoutCounter == payoutQty){
+                 msg.sender.transfer(this.balance);
+             } else { //else send only (initial amount funded divided by payoutQty)
+                 msg.sender.transfer(initialFunding/payoutQty);
+                 payoutCounter++;
+             }
+
         }
+    }
+
+    function getAnswersRemaining() public constant returns (uint answersRemaining){
+        answersRemaining = payoutQty - payoutCounter;
+    }
+
+    function getPayoutForAnswer() public constant returns (uint payoutForAnswer) {
+        if (payoutCounter == payoutQty) { payoutForAnswer = this.balance; }
+        else { payoutForAnswer = (initialFunding/payoutQty); }
     }
 }
